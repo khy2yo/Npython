60dabfb8ff0670b18d41344a0cbcf2a61235fe7d
khy2yo <104503828+khy2yo@users.noreply.github.com>
2022-05-30 00:28:41 +0900

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+print("5개의 정수를 입력하세요.")
+Integer1=int(input("정수 1의 값을 입력하시요.; "))
+Integer2=int(input("정수 2의 값을 입력하시요.; "))
+Integer3=int(input("정수 3의 값을 입력하시요.; "))  
+Integer4=int(input("정수 4의 값을 입력하시요.; "))
+integer5=int(input("정수 5의 값을 입력하시요.; "))
+
+sum = Integer1+Integer2+Integer3+Integer4+integer5
+
+Total = sum / 5
+
+print(sum)
+print(Total)
+                   
